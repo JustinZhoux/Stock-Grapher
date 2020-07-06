2c3a735b4fd151a166ac5fb52d49f12dc63d39c8
Justin Zhou <38168971+JustinZhoux@users.noreply.github.com>
2020-07-06 17:25:31 -0400

    Update setpu.py

--- setpu.py
+++ setpu.py
@@ -1,2 +1,3 @@
 import chart_studio
+# you have to initiate chart_studio by using your chart_studio username, and api_key. The one shown blow is mine
 chart_studio.tools.set_credentials_file(username='PoopingDog', api_key = "fRQhcPyYGc4u02KmLMDz")
